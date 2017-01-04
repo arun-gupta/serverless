dfd852f16b82b8003926cdea78899eddab52022e
Arun Gupta <arun.gupta@gmail.com>
2017-01-04 15:57:43 -0800

    adding methods for Lambda Proxy integration

--- aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/apigateway/APIGatewayUtil.java
+++ aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/apigateway/APIGatewayUtil.java
@@ -1,19 +1,31 @@
 package org.sample.serverless.aws.couchbase.apigateway;
 
+import com.couchbase.client.java.document.json.JsonArray;
 import com.couchbase.client.java.document.json.JsonObject;
+import java.util.Arrays;
 
 /**
  * @author arungupta
  */
 public class APIGatewayUtil {
+
     public static final String CONTENT_TYPE_JSON_HEADER = "Content-Type: application/json";
     public static final String CONTENT_TYPE_TEXT_HEADER = "Content-Type: text/plain";
+
+    public static final JsonObject proxyIntegration(int statusCode, String headers, String body) {
+        return proxyIntegration(statusCode, Arrays.asList(headers).toArray(new String[0]), body);
+    }
     
-    public static final String proxyIntegration(int statusCode, String headers, String body) {
-        return JsonObject.create()
-                .put("statusCode", statusCode)
-                .put("headers", headers)
-                .put("body", body)
-                .toString();
+    public static final JsonObject proxyIntegration(int statusCode, String[] headers, String body) {
+        JsonObject json = JsonObject.create();
+
+        json.put("statusCode", statusCode);
+        JsonArray array = JsonArray.create();
+        Arrays.stream(headers).forEach(header -> array.add(header));
+        json.put("headers", array);
+
+        json.put("body", body);
+
+        return json;
     }
 }
