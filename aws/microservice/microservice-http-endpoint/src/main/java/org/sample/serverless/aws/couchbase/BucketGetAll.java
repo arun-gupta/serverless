360023e08b0aa7a12ffd03fc79d0c42aba41ddea
Arun Gupta <arun.gupta@gmail.com>
2017-01-04 15:53:17 -0800

    invoking Lambda function directly

--- aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/BucketGetAll.java
+++ aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/BucketGetAll.java
@@ -4,7 +4,6 @@ import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.couchbase.client.java.query.N1qlQuery;
-import com.couchbase.client.java.query.N1qlQueryResult;
 import static com.couchbase.client.java.query.dsl.Expression.i;
 import org.sample.serverless.aws.couchbase.apigateway.APIGatewayUtil;
 import static com.couchbase.client.java.query.Select.select;
@@ -24,15 +23,18 @@ public class BucketGetAll implements RequestHandler<Book, String> {
                 .limit(10));
 //        N1qlQuery query = N1qlQuery.simple("SELECT * FROM " + CouchbaseUtil.getBucketName() + " LIMIT 10");
 
-        N1qlQueryResult result = CouchbaseUtil.getBucket(logger).query(query);
-        return result.allRows().toString();
+        String result = CouchbaseUtil.getBucket(logger).query(query).allRows().toString();
+        System.out.println("Lambda: " + result);
+
+        return result;
     }
 
     public static void main(String[] args) {
         N1qlQuery query = N1qlQuery.simple("SELECT * FROM " + CouchbaseUtil.getBucketName() + " LIMIT 10");
+        BucketGetAll bucket = new BucketGetAll();
 
-        String result = CouchbaseUtil.getBucket(null).query(query).allRows().toString();
-        System.out.println("Lambda: " + result);
+        String result = bucket.handleRequest(null, null);
+        System.out.println("Lambda main(): " + result);
         System.out.println("API Gateway: " + APIGatewayUtil.proxyIntegration(200, APIGatewayUtil.CONTENT_TYPE_JSON_HEADER, result));
     }
 }
