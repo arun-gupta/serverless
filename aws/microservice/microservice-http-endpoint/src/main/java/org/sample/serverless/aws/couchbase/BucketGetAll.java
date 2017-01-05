4512d693a32ff5420c26bc0ae9e40aa58aa8d847
Arun Gupta <arun.gupta@gmail.com>
2017-01-05 10:57:44 -0800

    removing redundant imports

--- aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/BucketGetAll.java
+++ aws/microservice/microservice-http-endpoint/src/main/java/org/sample/serverless/aws/couchbase/BucketGetAll.java
@@ -1,7 +1,6 @@
 package org.sample.serverless.aws.couchbase;
 
 import com.amazonaws.services.lambda.runtime.Context;
-import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.couchbase.client.java.query.N1qlQuery;
 import static com.couchbase.client.java.query.dsl.Expression.i;
