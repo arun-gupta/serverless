effce71fbced8921f1dc9cb394793664f637688f
Arun Gupta <arun.gupta@gmail.com>
2017-05-17 21:09:44 -0700

    Removing unused imports, code formatting

--- microservice/books/src/main/java/org/sample/aws/serverless/books/DynamoDBUtil.java
+++ microservice/books/src/main/java/org/sample/aws/serverless/books/DynamoDBUtil.java
@@ -1,8 +1,5 @@
 package org.sample.aws.serverless.books;
 
-import java.util.concurrent.TimeUnit;
-
-import com.amazonaws.client.builder.AwsClientBuilder;
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
@@ -13,32 +10,35 @@ import com.amazonaws.services.dynamodbv2.document.Table;
  * @author arungupta
  */
 public class DynamoDBUtil {
-	private static AmazonDynamoDB dynamodbClient;
-	private static Table table;
-	
-	public static AmazonDynamoDB getClient() {
-		if (null != dynamodbClient)
-			return dynamodbClient;
-		
-		String region = System.getenv("DYNAMODB_REGION");
-		if (null == region) {
-			System.err.println("Region is null, using default \"" + Regions.US_WEST_1 + "\"");
-			region = Regions.US_WEST_1.name();
-		}
-		System.out.println("DynamoDB region: " + region);
-		
-		dynamodbClient = AmazonDynamoDBClientBuilder.standard()
-	            .withRegion(region)
-	            .build();
+
+    private static AmazonDynamoDB dynamodbClient;
+    private static Table table;
+
+    public static AmazonDynamoDB getClient() {
+        if (null != dynamodbClient) {
+            return dynamodbClient;
+        }
+
+        String region = System.getenv("DYNAMODB_REGION");
+        if (null == region) {
+            System.err.println("Region is null, using default \"" + Regions.US_WEST_1 + "\"");
+            region = Regions.US_WEST_1.name();
+        }
+        System.out.println("DynamoDB region: " + region);
+
+        dynamodbClient = AmazonDynamoDBClientBuilder.standard()
+                .withRegion(region)
+                .build();
 
         return dynamodbClient;
-	}
-	
-	public static Table getTable() {
-		if (null != table)
-			return table;
-		
-		table = new DynamoDB(getClient()).getTable("Books");
-		return table;
-	}
+    }
+
+    public static Table getTable() {
+        if (null != table) {
+            return table;
+        }
+
+        table = new DynamoDB(getClient()).getTable("Books");
+        return table;
+    }
 }
